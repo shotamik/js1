8678e597f1c40bfaca0485f97ac5d45c85615e2f
shotamik <shotamikeladze@gmail.com>
2022-10-23 19:59:50 +0400

    added html and js files

---
+++ app.js
@@ -0,0 +1,29 @@
+let number = 5;
+let bookTitle = "Lord of the rings";
+let isSkyBlue = true;
+let myUndefined = undefined;
+let myNull = null;
+
+
+let num1 = 55;
+let num2 = 62;
+let myName = "Shota Mikeladze";
+let myHobby = "Coding";
+let yearOfBirth = 2001;
+let currentYear = 2022;
+let string2 = "My name is";
+const string3 = currentYear - yearOfBirth;
+const string1 = "I am";
+const string4 = "years old";
+const string5 = "and my hobby is";
+
+
+console.log(string2, myName);
+console.log(string1, string3, string4, string5, myHobby);
+
+const text1 = `My name is ${myName}
+${string1} ${currentYear - yearOfBirth} ${string4} ${string5} ${myHobby}`;
+
+
+
+console.log(text1);
